a701e845fb4d82c0a67fbc7f671f782681c452cd
jdmkd <jdmkd49@gmail.com>
2025-10-03 00:32:27 +0530

    feat(promotions): update promotion model with image support

--- promotions/models.py
+++ promotions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Promotion(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to="promotions/")
+    image = models.ImageField(upload_to="housingwalaa/promotions/")
     video_url = models.URLField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
